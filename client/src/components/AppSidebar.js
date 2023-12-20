e0de4c20a66d0b50bb58dba9d64f075c54296c97
John Cris <johncrismanabo9@gmail.com>
2023-12-20 08:50:47 +0800

    scan qrcode to download the app

--- client/src/components/AppSidebar.js
+++ client/src/components/AppSidebar.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-
 import { CImage, CSidebar, CSidebarBrand, CSidebarNav, CSidebarToggler } from '@coreui/react'
-
 import { AppSidebarNav } from './AppSidebarNav'
-
-import avatar from './../assets/images/logo-sm.png'
+import logo from './../assets/images/logo-sm.png'
+import qrcore from './../assets/images/qrcode.png'
 import SimpleBar from 'simplebar-react'
 import 'simplebar/dist/simplebar.min.css'
 import { InvalidTokenError, jwtDecode } from 'jwt-decode'
 // sidebar nav config
 import navigation from '../_nav'
+import app from './../assets/app/Oroqscholarship.apk'
+import QRCode from 'react-qr-code'
 
 const AppSidebar = () => {
   const dispatch = useDispatch()
@@ -42,7 +42,7 @@ const AppSidebar = () => {
       }}
     >
       <CSidebarBrand className="d-md-flex" to="/">
-        <CImage src={avatar} height={100} className="mt-3 mb-1" />
+        <CImage src={logo} height={100} className="mt-3 mb-1" />
       </CSidebarBrand>
       <CSidebarBrand className="d-md-flex" to="/">
         <p className="text-center h6">Oroquieta City Scholarship System</p>
@@ -51,7 +51,24 @@ const AppSidebar = () => {
         <SimpleBar>
           <AppSidebarNav items={navigation(user)} />
         </SimpleBar>
+        <CSidebarBrand className="d-md-flex bg-white  " to="/">
+          <div style={{ height: 'auto', margin: '0 auto', maxWidth: 100, width: '100%' }}>
+            <QRCode
+              size={200}
+              style={{ height: 'auto', maxWidth: '100%', width: '100%', marginTop: 20 }}
+              value={'https://oroquietacity.net/oroqscholarship' + app}
+              viewBox={`0 0 256 256`}
+            />
+          </div>
+        </CSidebarBrand>
+        <CSidebarBrand className="d-md-flex bg-white" to="/">
+          <p style={{ fontSize: 12, color: 'black' }} className="text-center">
+            Scan QR to download the app or click
+            <a href={app}> here.</a>
+          </p>
+        </CSidebarBrand>
       </CSidebarNav>
+
       <CSidebarToggler
         className="d-none d-lg-flex"
         onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
